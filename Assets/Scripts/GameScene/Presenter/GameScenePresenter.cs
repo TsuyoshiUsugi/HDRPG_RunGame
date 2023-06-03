87e33c1932e996bf8cacec8331ce5ffe5e2401be
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-03 09:51:55 +0900

    [update] ゲームシーンのReadyイベントを作成中

--- Assets/Scripts/GameScene/Presenter/GameScenePresenter.cs
+++ Assets/Scripts/GameScene/Presenter/GameScenePresenter.cs
@@ -7,12 +7,13 @@ using UnityEngine;
 /// </summary>
 public class GameScenePresenter : MonoBehaviour
 {
-    [SerializeField] 
+    [SerializeField] GameSceneManager _gameSceneManager;
+    [SerializeField] StartUI _startUI;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _gameSceneManager.ReadyStateEvent += () => StartCoroutine(nameof(StartUI.ShowStartUI));
     }
 
     // Update is called once per frame
