efe92b2a8e401922134b303bac31b4a353703ae2
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-13 20:09:27 +0900

    [add] インスペクタに表示できる辞書クラスを追加

--- Assets/Scripts/General/SerializeDictonary.cs
+++ Assets/Scripts/General/SerializeDictonary.cs
@@ -2,17 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SerializeDictonary : MonoBehaviour
+namespace Serialize
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Unityのインスペクター上に表示できる辞書型のクラス
+    /// シリアライズ可能なリストに辞書型自作クラスの各データを登録することで可能にしている
+    /// 使うときは継承して[System.Serializeble]を付けること
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <typeparam name="Type"></typeparam>
+    public class SerializeDictonary<TKey, TValue, Type> where Type : KeyAndValue<TKey, TValue>
     {
-        
+        [SerializeField] List<Type> _list;
+        Dictionary<TKey, TValue> _dict;
+
+        public Dictionary<TKey, TValue> GetTable ()
+        {
+            if (_dict == null)
+            {
+                _dict = ConvertListToDictionary(_list);
+            }
+            return _dict;
+        }
+
+        public List<Type> GetList()
+        {
+            return _list;
+        }
+
+        static Dictionary<TKey, TValue> ConvertListToDictionary(List<Type> list)
+        {
+            Dictionary<TKey, TValue> dic = new Dictionary<TKey, TValue> ();
+            foreach(KeyAndValue<TKey, TValue> pair in list)
+            {
+                dic.Add (pair.Key, pair.Value);
+            }
+
+            return dic;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    [System.Serializable]
+    public class KeyAndValue<Tkey, TValue>
     {
-        
+        public Tkey Key;
+        public TValue Value;
+
+        public KeyAndValue(Tkey key, TValue value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public KeyAndValue(KeyValuePair<Tkey, TValue> pair)
+        {
+            Key = pair.Key;
+            Value = pair.Value;
+        }
     }
+
 }
+
