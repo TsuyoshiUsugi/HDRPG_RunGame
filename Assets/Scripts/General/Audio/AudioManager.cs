5321a2d5f99692c7da060ccac43a6f9d1784970a
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-13 16:12:52 +0900

    [update] audioManager作成中

--- Assets/Scripts/General/Audio/AudioManager.cs
+++ Assets/Scripts/General/Audio/AudioManager.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class AudioManager : MonoBehaviour
+/// <summary>
+/// Audioを操作するマネージャクラス
+/// </summary>
+public class AudioManager : SingletonMonobehavior<AudioManager>
 {
+    [Header("設定値")]
+    [SerializeField] Dictionary<string, AudioClip> _audioDectionary;
+
+    AudioManager _audioManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TryGetComponent(out _audioManager);
+        SceneManager.sceneLoaded += OnChangeScene;
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// sceneが変わった時にBGMを変える処理
+    /// </summary>
+    /// <param name="nextScene"></param>
+    /// <param name="mode"></param>
+    private void OnChangeScene(Scene nextScene, LoadSceneMode mode)
     {
         
     }
