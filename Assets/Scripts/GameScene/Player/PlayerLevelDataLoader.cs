9913b663fcf41832becd02b10b90b1fceebd4f56
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-14 23:59:45 +0900

    [update] プレイヤーデータローダーにレベルを確認、取得する機能を追加

--- Assets/Scripts/GameScene/Player/PlayerLevelDataLoader.cs
+++ Assets/Scripts/GameScene/Player/PlayerLevelDataLoader.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerLevelDataLoader : MonoBehaviour
 {
+    [SerializeField] PlayerLevelData _playerLevelData;
+    [SerializeField] int _currentLevel = 1;
+    [SerializeField] int _currentlevelExp = 0;
+    [SerializeField] EachLevelData _currentLevelData;
+    string _getCurrentLevelKey = "CurrentLevel";
+    string _getCurrentLevelExpKey = "CurrentLevelExp";
+
+    public EachLevelData CurrentLevelData => _currentLevelData;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentLevel = PlayerPrefs.GetInt(_getCurrentLevelKey, 1);
+        _currentlevelExp = PlayerPrefs.GetInt(_getCurrentLevelExpKey, 0);
+        _currentLevelData = _playerLevelData.Data[_currentLevel - 1];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CheckLevelUp(int exp)
     {
-        
+        if (_playerLevelData.Data[_currentLevel - 1].RequireExp <= _currentlevelExp + exp)
+        {
+            var restExp = _currentlevelExp + exp - _playerLevelData.Data[_currentLevel - 1].RequireExp;
+            _currentLevel++;
+            _currentLevelData = _playerLevelData.Data[_currentLevel - 1];
+            PlayerPrefs.SetInt(_getCurrentLevelKey, _currentLevel);
+            PlayerPrefs.SetInt(_getCurrentLevelExpKey, restExp);
+            CheckLevelUp(restExp);
+        }
+        else
+        {
+            PlayerPrefs.SetInt(_getCurrentLevelExpKey, _currentlevelExp + exp);
+        }
     }
 }
