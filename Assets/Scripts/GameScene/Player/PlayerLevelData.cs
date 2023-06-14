7cb2313626bffad0ac39ba4585556a4bfaf7be31
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-14 19:51:49 +0900

    [update] データ仮完成

--- Assets/Scripts/GameScene/Player/PlayerLevelData.cs
+++ Assets/Scripts/GameScene/Player/PlayerLevelData.cs
@@ -7,6 +7,10 @@ public class PlayerLevelData : ScriptableObject
 {
     [SerializeField] List<EachLevelData> _levelData= new List<EachLevelData>();
     int _maxLevel = 20;
+    int _requireExpRate = 10;
+    int _hpIncreaseRate = 5;
+    float _speedIncreaseRate = 0.01f;
+    float _attackRateIncreaseRate = 0.01f;
 
     public int Maxlevel => _maxLevel;
     public List<EachLevelData> Data => _levelData;
@@ -19,13 +23,13 @@ public class PlayerLevelData : ScriptableObject
         for (int i = 1; i < _maxLevel; i++)
         {
             var level = i + 1;
-            var requireExp = 1;
-            var hp = _levelData[i - 1].Hp + 10;
-            var atk = _levelData[i - 1].Atk + 10;
-            var speed = _levelData[i - 1].Speed + 10;
-            var atkRate = _levelData[i - 1].AtkRate + 10;
-            var attackHitBoxPos = _levelData[i - 1].AttackHitBoxPos + new Vector3(0.1f, 0, 0);
-            var attackHitBoxSize = _levelData[i - 1].AttackHitBoxSize + new Vector3(0.1f, 0, 0);
+            var requireExp = _requireExpRate * i;
+            var hp = _levelData[i - 1].Hp + i / _hpIncreaseRate;
+            var atk = _levelData[i - 1].Atk + i;
+            var speed = _levelData[i - 1].Speed + _speedIncreaseRate;
+            var atkRate = _levelData[i - 1].AtkRate - _attackRateIncreaseRate;
+            var attackHitBoxPos = _levelData[i - 1].AttackHitBoxPos + new Vector3(0, 0, 0.1f);
+            var attackHitBoxSize = _levelData[i - 1].AttackHitBoxSize + new Vector3(0, 0, 0.1f);
 
             _levelData.Add(new EachLevelData(level, requireExp, hp, atk, speed, atkRate, attackHitBoxPos, attackHitBoxSize));
         }
@@ -39,8 +43,8 @@ public struct EachLevelData
     public int RequireExp;
     public int Hp;
     public int Atk;
-    public int Speed;
-    public int AtkRate;
+    public float Speed;
+    public float AtkRate;
     public Vector3 AttackHitBoxSize;
     public Vector3 AttackHitBoxPos;
 
@@ -55,7 +59,7 @@ public struct EachLevelData
     /// <param name="atkRate"></param>
     /// <param name="attackHitBoxSize"></param>
     /// <param name="attackHitBoxPos"></param>
-    public EachLevelData(int level, int requireExp , int hp, int atk, int speed, int atkRate, Vector3 attackHitBoxPos, Vector3 attackHitBoxSize)
+    public EachLevelData(int level, int requireExp , int hp, int atk, float speed, float atkRate, Vector3 attackHitBoxPos, Vector3 attackHitBoxSize)
     {
         Level = level;
         RequireExp = requireExp;
