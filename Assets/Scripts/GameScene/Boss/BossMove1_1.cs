e7467409bb8a81acdee0cb19718566689cf86702
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-15 04:31:51 +0900

    [update] ボス鳥のコライダーを正しく設定

--- Assets/Scripts/GameScene/Boss/BossMove1_1.cs
+++ Assets/Scripts/GameScene/Boss/BossMove1_1.cs
@@ -12,13 +12,15 @@ public class BossMove1_1 : MonoBehaviour, IEnemyMove
     [SerializeField] BossState _currentState = BossState.Float;
     [SerializeField] float _ground = 0;
     [SerializeField] float _switchStateTime = 10;
-    [SerializeField] float _restStateColSizeZ = 3.46f;
+    [SerializeField] float _restStateColSizeZ = 1.08f;
+    [SerializeField] float _restStateColCenterZ = -0.4f;
 
     float _currentStateTime = 0;
     float _originPos = 0;
     float _leftSide = 0;
     float _rightSide = 0;
     Vector3 _originColSize = Vector3.zero;
+    Vector3 _originalColCenter = Vector3.zero;
     BoxCollider _hitCollider;
 
     private void Start()
@@ -33,6 +35,7 @@ public class BossMove1_1 : MonoBehaviour, IEnemyMove
     {
         _hitCollider = GetComponent<BoxCollider>();
         _originColSize = _hitCollider.size;
+        _originalColCenter = _hitCollider.center;
         _originPos = transform.position.y;
         _currentState = BossState.Float;
         _leftSide = GameSceneManager.Instance.GetFieldInfo().leftSide;
@@ -85,6 +88,7 @@ public class BossMove1_1 : MonoBehaviour, IEnemyMove
             _currentStateTime += Time.deltaTime;
             transform.position -= Vector3.down * -Mathf.Abs(_speed) * Time.deltaTime;
             _hitCollider.size = new Vector3(_hitCollider.size.x, _hitCollider.size.y, _restStateColSizeZ);
+            _hitCollider.center = new Vector3(_hitCollider.center.x, _hitCollider.center.y, _restStateColCenterZ);
 
             if (transform.position.y <= _ground) transform.position =
                     new Vector3(transform.position.x, _ground, transform.position.z);
@@ -99,6 +103,7 @@ public class BossMove1_1 : MonoBehaviour, IEnemyMove
                 _currentStateTime = 0;
                 _currentState = BossState.Float;
                 _hitCollider.size = _originColSize;
+                _hitCollider.center = _originalColCenter;
             }
         }
     }
