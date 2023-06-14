2327fe38d98003edff34e8e9a5e93db124bfd264
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-14 13:43:31 +0900

    [add] ストーリーシーン用のインプットマネージャとpresenter作成

--- Assets/Scripts/Story/StoryInputManager.cs
+++ Assets/Scripts/Story/StoryInputManager.cs
@@ -1,15 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StoryInputManager : MonoBehaviour
 {
     [SerializeField] Button _skipButton;
+    [SerializeField] Button _optionButton;
+    public event Action OnSkipButtonClicked;
+    public event Action OnOptionButtonClicked;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _skipButton.onClick.AddListener(() => OnSkipButtonClicked?.Invoke());
+        _optionButton.onClick.AddListener(() => OnSkipButtonClicked?.Invoke());
     }
 }
