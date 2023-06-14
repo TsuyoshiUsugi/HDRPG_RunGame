672640321f1837f4676387a6262e052e171e7cc9
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-14 19:20:05 +0900

    [update] プレイヤーデータクラスに自動レベルデータ生成機能追加

--- Assets/Scripts/GameScene/Player/PlayerLevelData.cs
+++ Assets/Scripts/GameScene/Player/PlayerLevelData.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerLevelData", menuName = "ScriptableObjects/CreatePlayerLevelData")]
 public class PlayerLevelData : ScriptableObject
 {
-    public void Awake()
+    [SerializeField] List<EachLevelData> _data= new List<EachLevelData>();
+    int _maxlevel = 20;
+
+    private void Awake()
     {
-        Debug.Log("A");
+
+        _data.Add(new EachLevelData(1, 3, 1, 1, 1, new Vector3(1, 1, 1)));
+
+        for (int i = 1; i < _maxlevel; i++)
+        {
+            var level = i + 1;
+            var hp = _data[i - 1].Hp + 10;
+            var atk = _data[i - 1].Atk + 10;
+            var speed = _data[i - 1].Speed + 10;
+            var atkRate = _data[i - 1].AtkRate + 10;
+            var attackHitBoxSize = _data[i - 1].AttackHitBoxSize + new Vector3(0.1f, 0, 0);
+
+            _data.Add(new EachLevelData(level, hp, atk, speed, atkRate, attackHitBoxSize));
+        }
     }
 }
 
+[System.Serializable]
 public struct EachLevelData
 {
     public int Level;
