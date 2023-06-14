bf9974e6cff9aca92121044f237bed4d84144405
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-15 00:52:10 +0900

    [update] レベルアップ処理完成

--- Assets/Scripts/GameScene/Player/PlayerLevelDataLoader.cs
+++ Assets/Scripts/GameScene/Player/PlayerLevelDataLoader.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerLevelDataLoader : MonoBehaviour
@@ -8,17 +9,24 @@ public class PlayerLevelDataLoader : MonoBehaviour
     [SerializeField] int _currentLevel = 1;
     [SerializeField] int _currentlevelExp = 0;
     [SerializeField] EachLevelData _currentLevelData;
+    [SerializeField] bool _debug = false;
     string _getCurrentLevelKey = "CurrentLevel";
     string _getCurrentLevelExpKey = "CurrentLevelExp";
 
     public EachLevelData CurrentLevelData => _currentLevelData;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        _currentLevel = PlayerPrefs.GetInt(_getCurrentLevelKey, _currentLevel);
-        _currentlevelExp = PlayerPrefs.GetInt(_getCurrentLevelExpKey, 0);
+        if (!_debug)
+        {
+            _currentLevel = PlayerPrefs.GetInt(_getCurrentLevelKey, _currentLevel);
+            _currentlevelExp = PlayerPrefs.GetInt(_getCurrentLevelExpKey, 0);
+        }
+
+        if (_currentLevel > _playerLevelData.Maxlevel) _currentLevel = _playerLevelData.Maxlevel;
         _currentLevelData = _playerLevelData.Data[_currentLevel - 1];
+
     }
 
     public void CheckLevelUp(int exp)
